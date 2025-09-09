9add051a030718904dd4559a8c694aafa12f1a44
tuhu02 <230411100096@student.trunojoyo.ac.id>
2025-09-09 07:23:31 +0700

    belajar golang pertama kali

---
+++ latihan-variable/variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nama string = "Tuhu"
+	var umur int = 20
+	city := "Bojonegoro"
+	year := 2025
+
+	const pi = 3.14
+
+	fmt.Println("Nama:", nama)
+	fmt.Println("Umur: ", umur)
+	fmt.Println("Kota: ",city)
+	fmt.Println("Year: ",year)
+	fmt.Println("PI: ",pi)
+}
+
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Tuhu"
+	fmt.Println("Hello", name)
+}
+
